0583f2c4c706f3b2a5290fe36b1dd2c0f0db6948
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-04-25 08:56:40 +0300

    Завдання №2

--- samostiina_robota_2.cpp
+++ samostiina_robota_2.cpp
@@ -1,26 +1,31 @@
 ﻿#include <iostream>
 using namespace std;
 
-// Функція для обчислення суми двох чисел
-int add(int a, int b) {
-    return a + b;
+// Функція для обчислення факторіалу
+int factorial(int n) {
+    if (n < 0) {
+        cout << "Факторіал визначений лише для невід’ємних чисел." << endl;
+        return 0;
+    }
+
+    int result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
 }
 
+// Приклад використання
 int main() {
 
     system("chcp 1251 > nul"); //підключення української мови
 
-    int x, y;
+    int number;
+    cout << "Введіть число: ";
+    cin >> number;
 
-    // Ввід чисел з консолі
-    cout << "Введіть перше число: ";
-    cin >> x;
-
-    cout << "Введіть друге число: ";
-    cin >> y;
-
-    // Виклик функції та виведення результату
-    cout << "Сума: " << add(x, y) << endl;
+    int fact = factorial(number);
+    cout << "Факторіал " << number << " = " << fact << endl;
 
     return 0;
 }
