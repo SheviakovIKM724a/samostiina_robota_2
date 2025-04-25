15cd629e09e9dba8696674d86ae703d08e8cf3dd
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-04-25 09:22:23 +0300

    Завдання №7

--- samostiina_robota_2.cpp
+++ samostiina_robota_2.cpp
@@ -1,19 +1,42 @@
 ﻿#include <iostream>
 using namespace std;
 
-int sumArray(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; ++i) {
-        sum += arr[i];
+// Функція для сортування масиву за зростанням
+void sortArray(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Міняємо місцями елементи
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-    return sum;
+}
+
+// Функція для виведення масиву
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main() {
 
     system("chcp 1251 > nul"); //підключення української мови
 
-    int arr[] = { 1, 2, 3, 4, 5 };
-    cout << "Сума елементів масиву: " << sumArray(arr, 5) << endl;
+    int arr[] = { 5, 3, 8, 1, 2 };
+    int size = 5;  // Явно вказуємо розмір масиву
+
+    cout << "Масив до сортування: ";
+    printArray(arr, size);
+
+    sortArray(arr, size);
+
+    cout << "Масив після сортування: ";
+    printArray(arr, size);
+
     return 0;
 }
