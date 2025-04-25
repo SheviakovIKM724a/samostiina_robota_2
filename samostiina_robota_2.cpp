22e02cd7b06fa634407362c8dbd2846aa77aa70b
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-04-25 09:05:51 +0300

    Завдання №3

--- samostiina_robota_2.cpp
+++ samostiina_robota_2.cpp
@@ -1,31 +1,21 @@
 ﻿#include <iostream>
+
 using namespace std;
 
-// Функція для обчислення факторіалу
-int factorial(int n) {
-    if (n < 0) {
-        cout << "Факторіал визначений лише для невід’ємних чисел." << endl;
-        return 0;
+int getStringLength(const char* str) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
     }
-
-    int result = 1;
-    for (int i = 2; i <= n; ++i) {
-        result *= i;
-    }
-    return result;
+    return length;
 }
 
-// Приклад використання
 int main() {
 
     system("chcp 1251 > nul"); //підключення української мови
 
-    int number;
-    cout << "Введіть число: ";
-    cin >> number;
-
-    int fact = factorial(number);
-    cout << "Факторіал " << number << " = " << fact << endl;
-
+    char input[1000];
+    cin.getline(input, 1000);
+    cout << getStringLength(input) << endl;
     return 0;
 }
