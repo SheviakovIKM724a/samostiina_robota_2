dbfdbc3b792db2d7ee32eb0c1acdab6533c565a2
Євгеній Шевяков <evgenii.sheviakov@gmail.com>
2025-04-25 09:16:34 +0300

    Завдання №5

--- samostiina_robota_2.cpp
+++ samostiina_robota_2.cpp
@@ -1,37 +1,19 @@
 ﻿#include <iostream>
-
 using namespace std;
 
-bool isPrime(int num) {
-    // Відкидаємо всі числа менші або рівні 1
-    if (num <= 1) {
-        return false;
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
     }
-
-    
-    for (int i = 2; i < num; i++) {
-        if (num % i == 0) {
-            return false; 
-        }
-    }
-
-    return true; // Якщо жоден дільник не знайдений, то число просте
+    return sum;
 }
 
 int main() {
 
     system("chcp 1251 > nul"); //підключення української мови
 
-    int num;
-    cout << "Введіть ціле число: ";
-    cin >> num;
-
-    if (isPrime(num)) {
-        cout << num << " є простим числом." << endl;
-    }
-    else {
-        cout << num << " не є простим числом." << endl;
-    }
-
+    int arr[] = { 1, 2, 3, 4, 5 };
+    cout << "Сума елементів масиву: " << sumArray(arr, 5) << endl;
     return 0;
 }
